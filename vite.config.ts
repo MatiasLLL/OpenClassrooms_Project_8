8bd2aca3e0c5d83ffa752781e2882e2df89e9001
MatiasLLL <matias.larralde97@gmail.com>
2023-09-11 16:26:17 +0200

    modif vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import viteCompression from 'vite-plugin-compression';
 
+import { Plugin } from 'vite';
+
+interface VitePluginCompressionOptions {
+  verbose?: boolean;
+  disable?: boolean;
+  threshold?: number | { js?: number; css?: number };
+  algorithm?: 'gzip' | 'brotliCompress' | 'deflate' | 'deflateRaw';
+  ext?: string;
+}
+
+interface VitePluginCompression extends Plugin {
+  (options?: VitePluginCompressionOptions): Plugin;
+}
+
+declare const viteCompression: VitePluginCompression;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -9,10 +25,13 @@ export default defineConfig({
     viteCompression({
       verbose: true,
       disable: false,
-      threshold: 102400,
+      threshold: {
+        js: 1024000,
+        css: 10240
+      },
       algorithm: 'gzip',
       ext: '.gz'
-    })
+    }) as VitePluginCompression
   ],
   server: {
     port: 5000,
