d13115434adc7ef55c4dd6966b2c2255618f597e
MatiasLLL <matias.larralde97@gmail.com>
2023-09-11 12:43:43 +0200

    modif vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,20 @@
-import { defineConfig } from 'vite'
-// import ssr from 'vite-plugin-ssr'
-import react from '@vitejs/plugin-react-swc'
-import compression from 'vite-plugin-compression2'
-// import viteCompression from 'vite-plugin-compression';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import compression from 'vite-plugin-compression2';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // ssr({
-    //   onBeforeRoute(url, { pageContext }) {
-    //     pageContext.i18n = i18n.cloneInstance({ lng: pageContext.locale })
-    //   },
-    // }),
     react(),
-    viteCompression({
+    compression({
       verbose: true,
       disable: false,
       threshold: 10240,
       algorithm: 'gzip',
-      ext: '.gz'
-    })
+      ext: '.gz',
+    }),
   ],
   server: {
     port: 5000,
   },
-  build: {outDir: '.'}
-})
+  build: { outDir: '.' },
+});
