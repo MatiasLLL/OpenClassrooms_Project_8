11aadc7febaad2c0b21c8f59b289c8b6bc6efed8
MatiasLLL <matias.larralde97@gmail.com>
2023-09-11 16:37:21 +0200

    modif vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import viteCompression from 'vite-plugin-compression';
-
-import { Plugin } from 'vite';
-
-interface VitePluginCompressionOptions {
-  verbose?: boolean;
-  disable?: boolean;
-  threshold?: number | { js?: number; css?: number };
-  algorithm?: 'gzip' | 'brotliCompress' | 'deflate' | 'deflateRaw';
-  ext?: string;
-}
-
-interface VitePluginCompression extends Plugin {
-  (options?: VitePluginCompressionOptions): Plugin;
-}
-
-declare const viteCompression: VitePluginCompression;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteCompression({
-      verbose: true,
-      disable: false,
-      threshold: {
-        js: 10240000,
-        css: 10240
-      },
-      algorithm: 'gzip',
-      ext: '.gz'
-    }) as VitePluginCompression
+    // viteCompression({
+    //   verbose: true,
+    //   disable: false,
+    //   threshold: {
+    //     js: 10240000,
+    //     css: 10240
+    //   },
+    //   algorithm: 'gzip',
+    //   ext: '.gz'
+    // }) as VitePluginCompression
   ],
   server: {
     port: 5000,
   },
-  build: {outDir: 'public'}
+  build: {outDir: '.'}
 })
 
-// "build": "tsc && vite build",
 
 // algorithm: 'brotliCompress',
 // ext: '.br'
+
+
+// import viteCompression from 'vite-plugin-compression';
+
+// import { Plugin } from 'vite';
+
+// interface VitePluginCompressionOptions {
+//   verbose?: boolean;
+//   disable?: boolean;
+//   threshold?: number | { js?: number; css?: number };
+//   algorithm?: 'gzip' | 'brotliCompress' | 'deflate' | 'deflateRaw';
+//   ext?: string;
+// }
+
+// interface VitePluginCompression extends Plugin {
+//   (options?: VitePluginCompressionOptions): Plugin;
+// }
+
+// declare const viteCompression: VitePluginCompression;
